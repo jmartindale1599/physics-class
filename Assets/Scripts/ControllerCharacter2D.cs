3e1a7715b412d969e52d43da24ce22272db2cdab
jmartindale1599 <98477132+jmartindale1599@users.noreply.github.com>
2023-04-05 11:00:40 -0600

    added 2d
    I don't care for 2d but it is getting implemented yaaaay . . .

--- Assets/Scripts/ControllerCharacter2D.cs
+++ Assets/Scripts/ControllerCharacter2D.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using UnityEditor.Search;
 using UnityEngine;
 
-[RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(Rigidbody2D))]
 
-public class ControllerCharacter : MonoBehaviour{
+public class ControllerCharacter2D : MonoBehaviour{
 
 	[SerializeField] float speed;
 	
@@ -29,35 +29,31 @@ public class ControllerCharacter : MonoBehaviour{
     
 	[SerializeField] LayerMask groundLayerMask;
 
-    CharacterController characterController;
+	Rigidbody2D rb;
 	
-	Vector3 velocity = Vector3.zero;
+	Vector2 velocity = Vector2.zero;
 
 	void Start(){
 
-		characterController = GetComponent<CharacterController>();
+		rb = GetComponent<Rigidbody2D>();
 	
 	}
 
 	void Update(){
 
-        bool onGround = Physics.CheckSphere(groundTransform.position, 0.2f, groundLayerMask, QueryTriggerInteraction.Ignore);
+        bool onGround = Physics2D.OverlapCircle(groundTransform.position, 0.02f, groundLayerMask) != null;
 
         // get direction input
 
-        Vector3 direction = Vector3.zero;
+        Vector2 direction = Vector2.zero;
 		
 		direction.x = Input.GetAxis("Horizontal");
 		
-		direction.z = Input.GetAxis("Vertical");
-
 		// set velocity
 		
 		if (onGround){
 
             velocity.x = direction.x * speed;
-
-            velocity.z = direction.z * speed;
 
             if (velocity.y < 0) velocity.y = 0;
 
@@ -85,15 +81,9 @@ public class ControllerCharacter : MonoBehaviour{
 
         // move character
 
-        characterController.Move(velocity * Time.deltaTime);
+		rb.velocity = velocity;
 
-		Vector3 face = new Vector3(velocity.x, 0, velocity.z);
-
-		if(face.magnitude > 0){
-
-			transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(face), Time.deltaTime * turnRate);
-
-		}
+        //characterController.Move(velocity * Time.deltaTime);
 
 	}
 
@@ -122,41 +112,5 @@ public class ControllerCharacter : MonoBehaviour{
 		}
 
     }
-
-    void OnControllerColliderHit(ControllerColliderHit hit){
-
-		Rigidbody body = hit.collider.attachedRigidbody;
-
-		// no rigidbody
-		
-		if (body == null || body.isKinematic){
-
-			return;
-		
-		}
-
-		// We dont want to push objects below us
-		
-		if (hit.moveDirection.y < -0.3){
-
-			return;
-		
-		}
-
-		// Calculate push direction from move direction,
-		
-		// we only push objects to the sides never up and down
-		
-		Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
-
-		// If you know how fast your character is trying to move,
-		
-		// then you can also multiply the push velocity by that.
-
-		// Apply the push
-		
-		body.velocity = pushDir * hitForce;
-	
-	}
 
 }
